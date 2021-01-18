a475e0ff83c3871d76e17e270352a45e2f50febe
Burak Tunc <burak.tunc@check24.de>
2021-01-18 19:35:36 +0100

    Initial Commit

---
+++ AdmobDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AdmobDemo
+//
+//  Created by Burak Tunc on 18.01.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
